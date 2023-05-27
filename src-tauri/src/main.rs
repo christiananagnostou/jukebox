b835da25925b7d99b0c026fe8c7c8a54094205bc
George-Anagnostou <anagnostougeorgejohn@gmail.com>
2023-05-27 09:31:48 -0700

    add get_metadata to backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::command;
+use crate::metadata::Metadata;
+
+mod metadata;
+
+#[command]
+fn get_metadata(song_path: String) -> String {
+    let song_metadata = Metadata::build(song_path);
+    let song_metadata_json = serde_json::to_string(&song_metadata).unwrap();
+
+    song_metadata_json
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            get_metadata,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
