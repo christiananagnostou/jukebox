50c7f7b2d527799b3def5d0872bf754e29a860b6
ChristianAnagnostou <christian0722@gmail.com>
2023-06-14 22:26:37 -0700

    style albums page

--- src/routes/albums/index.tsx
+++ src/routes/albums/index.tsx
@@ -1,9 +1,11 @@
 import { component$, useComputed$, useContext, useSignal, useStore, useVisibleTask$ } from '@builder.io/qwik'
-import { ALBUM_ART_DB, StoreContext } from '../layout'
+import { ALBUM_ART_DB, StoreActionsContext, StoreContext } from '../layout'
 import { Store as DB } from 'tauri-plugin-store-api'
 import type { AlbumArt, ListItemStyle, Song } from '~/App'
 import { appWindow } from '@tauri-apps/api/window'
 import VirtualList from '~/components/Shared/VirtualList'
+import { ArrowDown } from '~/components/svg/ArrowDown'
+import { ArrowUp } from '~/components/svg/ArrowUp'
 
 interface Album {
   albumArtSRC: string
@@ -34,6 +36,7 @@ const AlbumArtDB = new DB(ALBUM_ART_DB)
 
 export default component$(() => {
   const store = useContext(StoreContext)
+  const storeActions = useContext(StoreActionsContext)
 
   const containerRef = useSignal<Element | undefined>()
 
@@ -41,8 +44,8 @@ export default component$(() => {
     virtualListHeight: 0,
     windowHeight: 0,
 
-    rowHeight: 500,
-    rowWidth: 300,
+    rowHeight: 420,
+    numCols: 5,
   })
 
   useVisibleTask$(async () => {
@@ -86,44 +89,134 @@ export default component$(() => {
     }
 
     // return chunk(Object.entries(a), Math.floor(state.rowWidth / (containerRef.value?.clientWidth || 1)))
-    return chunk(Object.entries(a), 5)
+    return chunk(Object.entries(a), state.numCols)
   })
 
   return (
-    <div class="overflow-auto">
-      <div class="flex-1 h-full" style={{ maxHeight: state.virtualListHeight + 'px' }} ref={containerRef}>
-        {albums.value && (
-          <VirtualList
-            numItems={albums.value.length}
-            itemHeight={state.rowHeight}
-            windowHeight={state.virtualListHeight || 0}
-            scrollToRow={store.libraryView.cursorIdx}
-            renderItem={component$(({ index, style }: { index: number; style: ListItemStyle }) => {
-              const row = albums.value[index]
+    <>
+      <div
+        class="w-full text-sm grid grid-cols-[22px_1fr_1fr_1fr_120px_120px_120px_70px] text-left items-center border-b border-gray-700"
+        style={{ height: 30 + 'px', paddingRight: 'var(--scrollbar-width)' }}
+      >
+        <span />
 
-              return (
-                <div class="w-full flex" style={{ ...style, height: state.rowHeight }}>
-                  {row.map(([albumName, { albumArtSRC, songs, artist, date }]) => (
-                    <div
-                      class="flex flex-col flex-1"
-                      key={albumName}
-                      style={{ width: state.rowWidth, maxWidth: state.rowWidth }}
-                    >
-                      <div class="min-w-full aspect-square bg-slate-800">
-                        {albumArtSRC && <img src={albumArtSRC} alt={albumName} width={250} height={250} />}
-                      </div>
-                      <span class="truncate">{albumName}</span>
-                      <span class="truncate">{songs.length}</span>
-                      <span class="truncate">{artist}</span>
-                      <span class="truncate">{date}</span>
-                    </div>
-                  ))}
-                </div>
-              )
-            })}
-          />
-        )}
+        <button
+          class="truncate h-full flex items-center justify-between pl-1 pr-2 relative"
+          onClick$={() => (store.sorting = store.sorting === 'title-desc' ? 'title-asc' : 'title-desc')}
+        >
+          Title
+          {store.sorting === 'title-desc' && <ArrowDown />}
+          {store.sorting === 'title-asc' && <ArrowUp />}
+        </button>
+
+        <button
+          class="border-l border-gray-700 truncate h-full flex items-center justify-between px-2 relative"
+          onClick$={() => (store.sorting = store.sorting === 'artist-desc' ? 'artist-asc' : 'artist-desc')}
+        >
+          Artist
+          {store.sorting === 'artist-desc' && <ArrowDown />}
+          {store.sorting === 'artist-asc' && <ArrowUp />}
+        </button>
+
+        <button
+          class="border-l border-gray-700 truncate h-full flex items-center justify-between px-2 relative"
+          onClick$={() => (store.sorting = store.sorting === 'album-desc' ? 'album-asc' : 'album-desc')}
+        >
+          Album
+          {store.sorting === 'album-desc' && <ArrowDown />}
+          {store.sorting === 'album-asc' && <ArrowUp />}
+        </button>
+
+        <button
+          class="border-l border-gray-700 truncate h-full flex items-center justify-between px-2 relative"
+          onClick$={() => (store.sorting = store.sorting === 'track-desc' ? 'track-asc' : 'track-desc')}
+        >
+          Track
+          {store.sorting === 'track-desc' && <ArrowDown />}
+          {store.sorting === 'track-asc' && <ArrowUp />}
+        </button>
+
+        <button
+          class="border-l border-gray-700 truncate h-full flex items-center justify-between px-2 relative"
+          onClick$={() => (store.sorting = store.sorting === 'hertz-desc' ? 'hertz-asc' : 'hertz-desc')}
+        >
+          Hertz
+          {store.sorting === 'hertz-desc' && <ArrowDown />}
+          {store.sorting === 'hertz-asc' && <ArrowUp />}
+        </button>
+
+        <button
+          class="border-l border-gray-700 truncate h-full flex items-center justify-between px-2 relative"
+          onClick$={() => (store.sorting = store.sorting === 'date-desc' ? 'date-asc' : 'date-desc')}
+        >
+          Date
+          {store.sorting === 'date-desc' && <ArrowDown />}
+          {store.sorting === 'date-asc' && <ArrowUp />}
+        </button>
+
+        <button
+          class="border-l border-gray-700 truncate h-full flex items-center justify-between px-2 relative"
+          onClick$={() => (store.sorting = store.sorting === 'fave-desc' ? 'fave-asc' : 'fave-desc')}
+        >
+          Fave
+          {store.sorting === 'fave-desc' && <ArrowDown />}
+          {store.sorting === 'fave-asc' && <ArrowUp />}
+        </button>
       </div>
-    </div>
+
+      <div class="overflow-auto">
+        <div class="flex-1 h-full" style={{ maxHeight: state.virtualListHeight + 'px' }} ref={containerRef}>
+          {albums.value && (
+            <VirtualList
+              numItems={albums.value.length}
+              itemHeight={state.rowHeight}
+              overscan={2}
+              windowHeight={state.virtualListHeight || 0}
+              scrollToRow={store.libraryView.cursorIdx}
+              renderItem={component$(({ index, style }: { index: number; style: ListItemStyle }) => {
+                const row = albums.value[index]
+
+                console.log(row)
+                // Row
+                return (
+                  <div
+                    class="w-full flex gap-4 px-4 pt-4 last:pb-4"
+                    style={{ ...style, height: state.rowHeight + 'px' }}
+                  >
+                    {row.map(([albumName, { albumArtSRC, songs, artist, date }]) => (
+                      // Column
+                      <div
+                        class="flex flex-col h-full flex-1 w-0 border border-slate-700 cursor-pointer"
+                        key={albumName}
+                        onDblClick$={() => {
+                          store.playlist = songs
+                          storeActions.playSong(songs[0], 0)
+                        }}
+                      >
+                        <div class="min-w-full aspect-square bg-slate-800">
+                          {albumArtSRC && (
+                            <img src={albumArtSRC} alt={albumName} width={250} height={250} class="w-full h-full" />
+                          )}
+                        </div>
+                        <div class="p-2 h-full">
+                          <span class="truncate py-1 block">{albumName}</span>
+                          <span class="truncate py-1 block">{songs.length}</span>
+                          <span class="truncate py-1 block">{artist}</span>
+                          <span class="truncate py-1 block">{date}</span>
+                        </div>
+                      </div>
+                    ))}
+
+                    {[...Array(state.numCols - row.length)].map((item) => (
+                      <div class="flex-1 w-0" key={item} />
+                    ))}
+                  </div>
+                )
+              })}
+            />
+          )}
+        </div>
+      </div>
+    </>
   )
 })
