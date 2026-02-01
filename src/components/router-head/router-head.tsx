1a2717bdea3498ef1631043db3345af608731048
ChristianAnagnostou <christian0722@gmail.com>
2026-01-31 23:57:21 -0800

    fix duplicate dangerouslySetInnerHTML in router head

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,12 +1,12 @@
-import { component$ } from '@builder.io/qwik';
-import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
+import { component$ } from '@builder.io/qwik'
+import { useDocumentHead, useLocation } from '@builder.io/qwik-city'
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
-  const head = useDocumentHead();
-  const loc = useLocation();
+  const head = useDocumentHead()
+  const loc = useLocation()
 
   return (
     <>
@@ -24,9 +24,11 @@ export const RouterHead = component$(() => {
         <link key={l.key} {...l} />
       ))}
 
-      {head.styles.map((s) => (
-        <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
-      ))}
+      {head.styles.map((s) => {
+        const { dangerouslySetInnerHTML: _deprecated, ...styleProps } = s.props || {}
+
+        return <style key={s.key} {...styleProps} dangerouslySetInnerHTML={s.style} />
+      })}
     </>
-  );
-});
+  )
+})
